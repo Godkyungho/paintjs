9d682bd2d9c8c86b08d5092be8fe8f5a4a3539c7
Godkyungho <98026101+Godkyungho@users.noreply.github.com>
2022-03-23 23:53:52 +0900

    0323_paintJs

--- app.js
+++ app.js
@@ -1,17 +1,37 @@
 const canvas = document.getElementById("jsCanvas")
 const ctx = canvas.getContext("2d")
+const colors = document.getElementsByClassName("jsColor")
+const range = document.getElementById("jsRange")
+const mode = document.getElementById("jsMode")
+
+const INITIAL_COLOR = "#2c2c2c"
+
+canvas.width = 700
+canvas.height = 700
+
+
+
+
+
+
 //default
-ctx.strokeStyle = "#2c2c2c"
+ctx.strokeStyle = INITIAL_COLOR
+ctx.fillStyle = INITIAL_COLOR
 ctx.lineWidth = 2.5;
 
+
 let painting = false;
+let filling = false;
+
 
 function stopPainting(){
     painting = false;
 }
 
 function startPainting(){
-    painting = true;
+    if(filling===false){
+        painting=true;
+    }
 }
 
 function onMouseMove(event){
@@ -24,11 +44,45 @@ function onMouseMove(event){
         ctx.lineTo(x,y)
         ctx.stroke()
     }
-
 }
 
-function onMouseDown(event){
-    painting = true;
+function handleColorClick(event){
+    const color = event.target.style.backgroundColor
+    ctx.strokeStyle = color;
+    ctx.fillStyle = color;
+}
+
+
+Array.from(colors).forEach(potato =>potato.addEventListener("click",handleColorClick))
+
+function handleRangeChange(event){
+    const size = event.target.value
+    ctx.lineWidth =size
+}
+
+function handleModeClick(){
+    if(filling===false){
+        filling=true;
+        mode.innerText ="PAINT"
+    }else{
+        filling=false;
+        mode.innerText="FILL"
+    }
+    console.log(filling)
+}
+
+function handleCanvasClick(){
+    if(filling){
+        ctx.fillRect(0,0,700,700)
+    }
+}
+
+if(range){
+    range.addEventListener("input",handleRangeChange)
+}
+
+if(mode){
+    mode.addEventListener("click",handleModeClick)
 }
 
 
@@ -38,4 +92,5 @@ if(canvas){
     canvas.addEventListener("mousedown",startPainting)
     canvas.addEventListener("mouseup",stopPainting)
     canvas.addEventListener("mouseleave",stopPainting)
+    canvas.addEventListener("click",handleCanvasClick)
 }
